ceb7dfeb4531ce4bc23a102fab81cd0abfce3551
Lothevy <45122731+ryno57@users.noreply.github.com>
2025-10-23 17:57:37 +0200

    Initial commit

---
+++ scripts/player.gd
@@ -0,0 +1,65 @@
+extends CharacterBody3D
+
+@onready var camera_mount: Node3D = $camera_mount
+@onready var animation_player: AnimationPlayer = $visuals/mixamo_base/AnimationPlayer
+@onready var visuals: Node3D = $visuals
+
+
+var SPEED = 2
+const JUMP_VELOCITY = 4.5
+
+var walking_speed = 2
+var running_speed = 4
+
+var running = false
+
+@export var sens_horizontal = 0.5
+@export var sens_vertical = 0.5
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * sens_horizontal))
+		visuals.rotate_y(deg_to_rad(event.relative.x * sens_horizontal))
+		camera_mount.rotate_x(deg_to_rad(-event.relative.y * sens_vertical))
+
+func _physics_process(delta: float) -> void:
+	
+	if Input.is_action_pressed("run"):
+		SPEED = running_speed
+		running = true
+	else:
+		SPEED = walking_speed
+		running = false
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("left", "right", "forward", "backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		if animation_player.current_animation != "walking" and not running:
+			animation_player.play("walking")
+			
+		if animation_player.current_animation != "running" and running:
+			animation_player.play("running")
+			
+			
+		visuals.look_at(position + direction)
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		if animation_player.current_animation != "idle":
+			animation_player.play("idle")
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
